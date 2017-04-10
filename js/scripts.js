866e09651fb6494b97b26d0e78ca870c382acdd1
Ben Williams and Dana Weiss <ben@>
2017-04-10 09:53:45 -0700

    add jscript framework

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+//*--Business Logic--*//
+//Mode of Transportation//
+function Mode() {
+  this.walk = 0;
+  this.bike = 0;
+  this.oldTruck = ;
+  this.smallPickup = smallPickup;
+  this.standard = standardPickup
+  this.hybrid = hybrid;
+  this.sedan = sedan;
+}
+
+//Prototype for Mode of Transportation//
+Mode.prototype.getModeCarbonTotal = function() {
+  inputtedMiles * theModeFunction;
+
+  //User inputs miles by mode. we multiple the miles by the corresponding multiplier for the mode.
+}
+
+
+//*--User Interface--*//
+$(document).ready(function() {
+
+});
+
+
+// *Reference * //
+// Transportation
+
+// Vehicles:
+// 2012 truck: 15mpg with 1.32 lbs co2/mi
+// newer hybrids: 50mpg avg with 0.39 lbs co2/mi
+// new sedans/wagons: 23.7 mpg avg with 0.84 lbs co2/mi
+// small pickup: 21 mpg avg with 0.94 lbs co2/mi
+// standard pickup: 18 mpg avg with 1.1 lbs co2/mi
+
+// Public Transit
+// // lightrail: 0.36 lbs co2/mi; 0.14 if full occupancy
+// bus: 0.56 lbs co2/mi; 0.18 if full occupancy
+//
+// Transportation co2 reduction techniques:
+// walk or ride a bicycle
+// take the bus or lightrail
+// perform regular maintenance on vehicle: $214 annual savings and 1,157 lbs co2
+// reduce drive miles: proportional to mpg.
+//
