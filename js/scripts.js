e5fcc1fe77045cf5fe0d3770ff974ff12c22d74f
Ben Williams and Dana Weiss <dana@>
2017-04-10 16:25:45 -0700

    add functioning selectors and functions for recycling and home energy

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 //*--Business Logic--*//
 //Mode of Transportation//
-function Economic(mode, miles) {
+function Economic(mode, miles, electric, natGas, fuel) {
   this.mode = mode;
   this.miles = miles;
+  this.electric = electric;
+  this.natGas = natGas;
+  this.fuel = fuel;
+  this.recyclingArray = [];
 }
 
 //Prototype for Mode of Transportation//
@@ -11,6 +15,27 @@ Economic.prototype.modeCarbonTotal = function(mode, miles) {
   return transportProduct;
 }
 
+Economic.prototype.homeCarbonTotal = function(electric) {
+  var electricEnergyProduct = this.electric * 10.546;
+  var natGasProduct = (this.natGas - 9.5) * 8.846;
+  var fuelProduct = this.fuel * 7.59
+  var homeEnergyProduct = electricEnergyProduct + natGasProduct + fuelProduct;
+  return homeEnergyProduct;
+}
+
+//natural gas
+// take the price of the bill, subtract 9.50, multiply by .7323. that's how many therms 12.08 pounds per therm. src: nwNatural
+
+// fuel oil
+// 22.40lbsCO2/gallon, $2.95 per gallon (if you buy 100 gallons)
+//http://best-heating-oil.com/heating-oil-pricing/
+
+//electricity
+// 8.63 kWh per $1 spent in a day, 1.222 lbs co2 per/kwh https://carbonfund.org/how-we-calculate/    https://www.epa.gov/sites/production/files/2015-10/documents/egrid2012_summarytables_0.pdf PG&E customer bill portland
+
+
+
+
 //*--User Interface--*//
 $(document).ready(function() {
   var transportation = new Economic();
@@ -18,15 +43,18 @@ $(document).ready(function() {
     e.preventDefault();
     var inputtedMode = $("#mode").val();
     var inputtedMiles = $("#miles").val();
-    var getCarbonTotal = new Economic(inputtedMode, inputtedMiles);
+    var inputtedElectric = $("#electricity").val();
+    var inputtedNatGas = $("#natgas").val();
+    var inputtedFuel = $("#fuel").val();
+    $("input:checkbox[name=recycling]:checked").each(function(){
+        let inputtedRecycling = parseFloat($(this).val());
+        transportation.recyclingArray.push(inputtedRecycling);
+    });
+    var getCarbonTotal = new Economic(inputtedMode, inputtedMiles, inputtedElectric, inputtedNatGas, inputtedFuel);
     getCarbonTotal.modeCarbonTotal();
+    getCarbonTotal.homeCarbonTotal();
     console.log(getCarbonTotal.modeCarbonTotal());
+    console.log(getCarbonTotal.homeCarbonTotal());
+    console.log(transportation.recyclingArray);
   });
 });
-
-
-// *Reference * //
-// Transportation
-// Public Transit
-// // lightrail: 0.36 lbs co2/mi; 0.14 if full occupancy
-// bus: 0.56 lbs co2/mi; 0.18 if full occupancy
