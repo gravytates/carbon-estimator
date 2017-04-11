ce9a43aab9102bf4007e19be740b692112e760be
Dana Weiss and Grady L Shelton <grady@>
2017-04-11 15:41:16 -0700

    add result styling. change formulas to scale them to a monthly output. add html element min zero to prevent negative values

--- js/scripts.js
+++ js/scripts.js
@@ -15,14 +15,14 @@ function Economic(mode, miles, electric, natGas, fuel, wasteProduct) {
 Economic.prototype.modeCarbonTotal = function(mode, miles) {
   var transportProduct = this.mode * this.miles;
   console.log("trans: " + transportProduct);
-  return transportProduct;
+  return (transportProduct * 4.33);
 }
 
 Economic.prototype.homeCarbonTotal = function(electric) {
-  var electricEnergyProduct = this.electric * 10.546;
+  var electricEnergyProduct = this.electric * 10.55;
   if (this.natGas <= 9.5) {this.natGas = 9.5};
-  var natGasProduct = (this.natGas - 9.5) * 8.846;
-  var fuelProduct = this.fuel * 7.59
+  var natGasProduct = (this.natGas - 9.5) * 8.85;
+  var fuelProduct = this.fuel * 7.59;
   var homeEnergyProduct = electricEnergyProduct + natGasProduct + fuelProduct;
   console.log("home: " + homeEnergyProduct);
   return homeEnergyProduct;
@@ -35,7 +35,7 @@ Economic.prototype.recyclingTotal = function() {
     wasteProduct -= recyclable;
     console.log("recycle: " + wasteProduct);
   });
-  return wasteProduct;
+  return (wasteProduct / 12);
 };
 
 // this works as of 10am 4/11
@@ -50,8 +50,7 @@ Economic.prototype.foodTotal = function() {
 
 Economic.prototype.overallTotal = function(mode, miles, electric, natGas, fuel, recyclingArray, foodArray) {
   var bigResult = this.modeCarbonTotal(mode, miles) + this.homeCarbonTotal(electric, natGas, fuel) + this.recyclingTotal(recyclingArray) + this.foodTotal(foodArray);
-  // console.log(this.recyclingTotal(recyclingArray));
-  return bigResult;
+  return bigResult.toFixed(2);
 }
 
 //*--User Interface--*//
@@ -74,15 +73,13 @@ $(document).ready(function() {
       getCarbonTotal.foodArray.push(inputtedFood);
     });
 
-
     getCarbonTotal.modeCarbonTotal();
     getCarbonTotal.homeCarbonTotal();
     recycleNFood.foodTotal();
     finalResult = getCarbonTotal.overallTotal(inputtedMode, inputtedMiles, inputtedElectric, inputtedNatGas, inputtedFuel);
-    $("#finalTotal").text(finalResult);
+    $("#finalTotal").html("You are responsible for emitting " + finalResult + 'lbs of CO<sub>2</sub> per month!');
     console.log("final total: " + finalResult);
     console.log(getCarbonTotal.modeCarbonTotal());
     console.log(getCarbonTotal.homeCarbonTotal());
-    // console.log(recycleNFood.recyclingArray);
   });
 });
